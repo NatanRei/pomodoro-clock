40e803cb888423996bbf82bb0c18e7bc830f14a9
NatanRei <nreischmura@gmail.com>
2023-01-08 21:38:33 -0300

    feat: more actualizations on styles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,28 @@
+import { Play } from 'phosphor-react'
+import { CountdownContainer, FormContainer, HomeContainer } from './styles'
+
 export function Home() {
-  return <>1234</>
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input type="text" id="task" />
+          <label htmlFor="minutesAmount">Password</label>
+          <input type="number" id="minutesAmount" />
+          <span>minutos.</span>
+        </FormContainer>
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+        <button type="submit">
+          <Play size={24} /> Começar
+        </button>
+      </form>
+    </HomeContainer>
+  )
 }
